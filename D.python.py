b803ce4e757cc3159ff6fe6bed180b055bc07cce
Santiago Cervantes <santicervantes1105@gmail.com>
2025-09-20 11:56:27 -0500

    Add files via upload

---
+++ D.python.py
@@ -0,0 +1,49 @@
+#Condicionales
+'''
+user_pago = input("¿Usted pago el plan? Si/NO")
+if user_pago == Si:
+else:
+
+
+
+
+'''
+#_______________________________________________
+
+
+
+google_one = input("Ingrese el plan seleccionado: ")
+espacio_google = input("Ingres su espacio ocupado en Google Drive: ")
+free_user = int(espacio_google) > 15 and google_one == "Ninguno"
+free_user = int(espacio_google) < 15 and google_one == "Ninguno"
+print("Su plan no puede almacenar más. Prueba otros plane")
+if free_user < 15: 
+    print("Estos son los planes: Basic(1$), Standar(2$), Premium(3$)")
+    input("Escoja el plan que desea: ")
+    
+i#f free_user = 
+
+
+if user_pago == "Si":
+    if int(espacio_google) > 15 and google_one == "Ninguno":
+        print("Debe eliminar archivos para poder trabajar")
+
+    elif int(espacio_google) > 100 and google_one == "Basic":
+        print("Debe eliminar archivos para poder trabajar")
+
+    elif int(espacio_google) > 200 and google_one == "Standar":
+        print("Debe eliminar archivos para poder trabajar")
+
+    elif int(espacio_google) > 2000 and google_one == "Premium":
+        print("Debe eliminar archivos para poder trabajar")
+
+    else:
+        print("Usted puede crear mas archivos")
+else:
+
+   
+    print("Paga para mas espacio 😭")
+     
+
+
+
